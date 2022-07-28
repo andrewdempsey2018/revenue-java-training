1885ba7bd440fb75d4d83e10c6067a39ba64e321
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-07-28 21:43:43 +0100

    add list of screenings and iterate

--- src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
+++ src/main/java/com/accenture/tagtrainingspring/TagTrainingSpringApplication.java
@@ -8,46 +8,59 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class TagTrainingSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TagTrainingSpringApplication.class, args);
-		printWelcomeMessage();
+    public static void main(String[] args) {
+        SpringApplication.run(TagTrainingSpringApplication.class, args);
+        printWelcomeMessage();
 
-		// create some test patients
-		Patient p = new Patient("John", 123, LocalDate.of(90,12,4), Gender.MALE);
-		Patient p2 = new Patient("Mick", 777, LocalDate.of(85,11,20), Gender.MALE);
-		Patient p3 = new Patient("Steve", 367, LocalDate.of(91,1,3), Gender.MALE);
-		Patient p4 = new Patient("Mary", 111, LocalDate.of(99,5,10), Gender.FEMALE);
+        // create some test patients
+        Patient p = new Patient("John", 123, LocalDate.of(90, 12, 4), Gender.MALE);
+        Patient p2 = new Patient("Mick", 777, LocalDate.of(85, 11, 20), Gender.MALE);
+        Patient p3 = new Patient("Steve", 367, LocalDate.of(91, 1, 3), Gender.MALE);
+        Patient p4 = new Patient("Mary", 111, LocalDate.of(99, 5, 10), Gender.FEMALE);
 
-		//create some test screenings
-		Screening s = new Screening(456, p, LocalDate.of(22,7,22), false);
-		Screening s2 = new Screening(543, p2, LocalDate.of(23,7,22), true);
-		Screening s3 = new Screening(876, p3, LocalDate.of(24,7,22), false);
-		Screening s4 = new Screening(675, p4, LocalDate.of(25,7,22), true);
+        //create some test screenings
+        Screening s = new Screening(456, p, LocalDate.of(22, 7, 22), false);
+        Screening s2 = new Screening(543, p2, LocalDate.of(23, 7, 22), true);
+        Screening s3 = new Screening(876, p3, LocalDate.of(24, 7, 22), false);
+        Screening s4 = new Screening(675, p4, LocalDate.of(25, 7, 22), true);
 
-		System.out.println(p.getName() + " has a malignant diagnosis of " + s.isMalignant());
+        // testing the ScreeningService class
+        System.out.println(p.getName() + " has a malignant diagnosis of " + s.isMalignant());
+        ScreeningService ss = new ScreeningService();
 
-		ScreeningService ss = new ScreeningService();
+        System.out.println("Screening " + ss.forPatient(s, p2));
 
-		boolean x = ss.forPatient(s,p2);
+        // create list to hold some screenings
+        List<Screening> screenList = new ArrayList<Screening>();
 
-		System.out.println(x);
+        screenList.add(s);
+        screenList.add(s2);
+        screenList.add(s3);
+        screenList.add(s4);
 
-	}
+        for (int i = 0; i < screenList.size(); i++) {
 
-	private static void printWelcomeMessage() {
-		System.out.println("\n" +
-				"  _______       _____   _______        _       _             \n" +
-				" |__   __|/\\   / ____| |__   __|      (_)     (_)            \n" +
-				"    | |  /  \\ | |  __     | |_ __ __ _ _ _ __  _ _ __   __ _ \n" +
-				"    | | / /\\ \\| | |_ |    | | '__/ _` | | '_ \\| | '_ \\ / _` |\n" +
-				"    | |/ ____ \\ |__| |    | | | | (_| | | | | | | | | | (_| |\n" +
-				"    |_/_/    \\_\\_____|    |_|_|  \\__,_|_|_| |_|_|_| |_|\\__, |\n" +
-				"                                                        __/ |\n" +
-				"                                                       |___/ \n");
-	}
+            System.out.println("Screening for patient " + screenList.get(i).getPatient().getName() + ", returned a malignant result of " + screenList.get(i).isMalignant());
+        }
+
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("\n" +
+                "  _______       _____   _______        _       _             \n" +
+                " |__   __|/\\   / ____| |__   __|      (_)     (_)            \n" +
+                "    | |  /  \\ | |  __     | |_ __ __ _ _ _ __  _ _ __   __ _ \n" +
+                "    | | / /\\ \\| | |_ |    | | '__/ _` | | '_ \\| | '_ \\ / _` |\n" +
+                "    | |/ ____ \\ |__| |    | | | | (_| | | | | | | | | | (_| |\n" +
+                "    |_/_/    \\_\\_____|    |_|_|  \\__,_|_|_| |_|_|_| |_|\\__, |\n" +
+                "                                                        __/ |\n" +
+                "                                                       |___/ \n");
+    }
 
 }
