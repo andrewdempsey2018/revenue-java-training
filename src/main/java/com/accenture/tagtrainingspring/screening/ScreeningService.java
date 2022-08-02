151bd0cf888645ef5db4ca0cd69e674d05a4f909
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2022-08-02 21:13:05 +0100

    add screenings endpoint

--- src/main/java/com/accenture/tagtrainingspring/screening/ScreeningService.java
+++ src/main/java/com/accenture/tagtrainingspring/screening/ScreeningService.java
@@ -1,11 +1,15 @@
 package com.accenture.tagtrainingspring.screening;
 
 import com.accenture.tagtrainingspring.patient.Patient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ScreeningService {
 
+    @Autowired
     ScreeningDatabase screeningDatabase;
 
     public ScreeningService(ScreeningDatabase s) {
